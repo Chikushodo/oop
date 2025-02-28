6ee8dbb76de3d65ddd9ee7b754831ccdd7912fb1
Chikushodo <149254988+Chikushodo@users.noreply.github.com>
2025-02-28 19:31:07 +0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,92 +1,51 @@
 #include <iostream>
 #include <string>
-using namespace std;
-class CombatUnit {
-private:
-    int health;
-    int damage;
-    string unitType;
 
-    static int totalHealth;
-    static int totalDamage;
+using namespace std;
+
+class Animal {
+private:
+    string name;
+    int weight;
+
+protected:
+    string species; 
 
 public:
-    CombatUnit(int health, int damage, std::string unitType) : health(health), damage(damage), unitType(unitType) {
-        totalHealth += health;
-        totalDamage += damage;
-        cout << "Создана боевая единица типа: " << unitType << endl;
-        cout << "Общее здоровье армии: " << totalHealth << endl;
-        cout << "Общая боевая мощь армии: " << totalDamage << endl;
+
+    Animal(string name, int weight, string species) : name(name), weight(weight), species(species) {}
+
+
+    void print() const {
+        cout << "Имя: " << name << ", Вес: " << weight << ", Вид: " << species << endl;
     }
 
-    ~CombatUnit() {
-        totalHealth -= health;
-        totalDamage -= damage;
-        cout << "Уничтожена боевая единица типа: " << unitType << endl;
-        cout << "Общее здоровье армии: " << totalHealth << endl;
-        cout << "Общая боевая мощь армии: " << totalDamage << endl;
-    }
-
-    int getHealth() const { return health; }
-    int getDamage() const { return damage; }
-    string getUnitType() const { return unitType; }
-    static int getTotalHealth() { return totalHealth; }
-    static int getTotalDamage() { return totalDamage; }
-
-    void setHealth(int newHealth) {
-        totalHealth -= health; 
-        health = newHealth;
-        totalHealth += health; 
-        cout << "Здоровье " << unitType << " изменено.  Новое значение: " << health << endl;
-        cout << "Общее здоровье армии: " << totalHealth << endl;
-    }
-
-    void setDamage(int newDamage) {
-        totalDamage -= damage; 
-        damage = newDamage;
-        totalDamage += damage; 
-        cout << "Урон " << unitType << " изменен. Новое значение: " << damage << endl;
-        cout << "Общая боевая мощь армии: " << totalDamage << endl;
-    }
-
-    void takeDamage(int damageTaken) {
-        int prevHealth = health;
-        health -= damageTaken;
-        if (health < 0) {
-            health = 0;
-        }
-        totalHealth -= (prevHealth - health); 
-        cout << unitType << " получил " << damageTaken << " урона.  Осталось здоровья: " << health << endl;
-        cout << "Общее здоровье армии: " << totalHealth << endl;
-
-        if (health == 0) {
-            cout << unitType << " уничтожен!" << endl;
-        }
-    }
-
-    void dealDamage(CombatUnit& target) {
-        cout << unitType << " наносит " << damage << " урона " << target.unitType << endl;
-        target.takeDamage(damage);
-    }
+    string getName() const { return name; }
+    int getWeight() const { return weight; }
 };
-int CombatUnit::totalHealth = 0;
-int CombatUnit::totalDamage = 0;
+
+class Cat : public Animal {
+public:
+    Cat(string name, int weight) : Animal(name, weight, "Кошка") {}
+
+};
+
+class Dog : public Animal {
+public:
+    Dog(string name, int weight) : Animal(name, weight, "Собака") {}
+};
 
 int main() {
-    CombatUnit unit1(100, 20, "Warrior");
-    CombatUnit unit2(80, 15, "Archer");
+    Cat cat1("Муська", 4);
+    Cat cat2("Барсик", 5);
+    Dog dog1("Бобик", 15);
+    Dog dog2("Рекс", 20);
 
-    cout << "Общее здоровье армии: " << CombatUnit::getTotalHealth() << endl;
-    cout << "Общая боевая мощь армии: " << CombatUnit::getTotalDamage() << endl;
+    cout << "Информация о кошках:" << endl;
+    cat1.print();
+    cat2.print();
 
-    unit1.setDamage(25);
-    unit2.setHealth(90);
-
-    unit1.dealDamage(unit2);
-
-    unit2.takeDamage(50);
-    unit2.dealDamage(unit1);
-
-    CombatUnit* unit3 = new CombatUnit(120, 30, "Tank");
-    delete unit3; 
+    cout << "\nИнформация о собаках:" << endl;
+    dog1.print();
+    dog2.print();
 }
